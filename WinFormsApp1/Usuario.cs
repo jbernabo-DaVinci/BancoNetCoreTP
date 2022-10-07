b11f97e874d9b4c003fd718847bcf533568d39b4
juancheDavinci <juan.bernabo@davinci.edu.ar>
2022-10-07 20:16:11 -0300

    updates added

--- WinFormsApp1/Usuario.cs
+++ WinFormsApp1/Usuario.cs
@@ -7,8 +7,16 @@ using System.Threading.Tasks;
 namespace WinFormsApp1 {
 	public class Usuario {
 		public int id { get; set; }
+		public int dni { get; set; }
 		public string nombre { get; set; }
 		public string pass { get; set; }
+		public string mail { get; set; }
+		public int intentosFallidos { get; set; }
+		public bool bloqueado { get; set; }
+		private List<CajaAhorro> cajasAhorro;
+		private List<PlazoFijo> plazosFijos;
+		private List<TarjetaCredito> tarjetasCredito;
+		private List<Pago> pagos;
 
 		public Usuario() { }
 
