f9e760316c46d693696e5ddbeb7f17a82aa5f207
JuanBer <juan.bernabo@wearepsh.com>
2022-10-07 18:24:43 -0300

    improvements to bank and user classes added

--- WinFormsApp1/Usuario.cs
+++ WinFormsApp1/Usuario.cs
@@ -4,25 +4,21 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace WinFormsApp1
-{
-    public class Usuario
-    {
-        public int id { get; set; }
-        public string nombre { get; set; }
-        public string pass { get; set; }
+namespace WinFormsApp1 {
+	public class Usuario {
+		public int id { get; set; }
+		public string nombre { get; set; }
+		public string pass { get; set; }
 
-        public Usuario() { }
+		public Usuario() { }
 
-        public Usuario(string Nombre, string Pass)
-        {
-            nombre = Nombre;
-            pass = Pass;
-        }
+		public Usuario(string nombre, string pass) {
+			this.nombre = nombre;
+			this.pass = pass;
+		}
 
-        public string[] toArray()
-        {
-            return new string[] { nombre, pass };
-        }
-    }
+		public string[] toArray() {
+			return new string[] { this.nombre, this.pass };
+		}
+	}
 }
