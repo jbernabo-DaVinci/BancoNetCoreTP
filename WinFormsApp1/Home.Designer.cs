b8e36b54e276d5b073aa260fa374ab5bbf56a74a
juancheDavinci <juan.bernabo@davinci.edu.ar>
2022-10-14 22:53:28 -0300

    changing the way to call to action

--- WinFormsApp1/Home.Designer.cs
+++ WinFormsApp1/Home.Designer.cs
@@ -170,7 +170,7 @@
             this.button4.TabIndex = 34;
             this.button4.Text = "Ver Detalles";
             this.button4.UseVisualStyleBackColor = false;
-            this.button4.Click += new System.EventHandler(this.onDetalleCajaAhorro);
+            this.button4.Click += new System.Windows.Forms.DataGridViewCellEventHandler(this.onDetalleCajaAhorro);
 
             // 
             // dataGridView1
