aef8b7d8defe9322f7d4b9ede9efb54d318f1f92
Francisco Rivas <francisco.rivas@davinci.edu.ar>
2022-11-29 15:41:59 -0300

    Se agregó el constructor TarjetaCredito para la conexión con la BD, con los 7 argumentos que corresponden a las 7 columnas de la tabla TarjetaCredito en la BD

--- WinFormsApp1/TarjetaCredito.cs
+++ WinFormsApp1/TarjetaCredito.cs
@@ -13,6 +13,7 @@ namespace WinFormsApp1 {
 		public float limite { get; set; }
 		public float consumo { get; set; }
 		public bool borrado { get; set; }
+		public int idUsuario { get; set }
 
 		public Usuario titular;
 
@@ -30,7 +31,20 @@ namespace WinFormsApp1 {
 			this.titular = titular;
 		}
 
-		public string[] toArray()
+        //Constructor para la conexión con BD
+        public TarjetaCredito(int id, int codigoV, int numero, float limite, float consumo, Boolean borrado, int idUsuario)
+        {
+            this.id = id;
+            Random rd = new Random();
+            this.numero = rd.Next(1000, 9999) + " " + rd.Next(1000, 9999) + " " + rd.Next(1000, 9999) + " " + rd.Next(1000, 9999);
+            this.codigoV = rd.Next(100, 999);
+            this.limite = limite;
+            this.consumo = 0;
+			this.borrado = borrado; 
+			this.idUsuario = idUsuario;
+        }
+
+        public string[] toArray()
 		{
 			return new string[] {
 				this.numero,
