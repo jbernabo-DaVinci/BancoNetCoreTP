60041313d3174319ed3bc8a0a6a2626fe562d79d
juancheDavinci <juan.bernabo@davinci.edu.ar>
2022-11-29 17:32:34 -0300

    bad merge fixed

--- WinFormsApp1/PlazoFijo.cs
+++ WinFormsApp1/PlazoFijo.cs
@@ -1,40 +1,20 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
-namespace WinFormsApp1 {
-	public class PlazoFijo {
-		public int id { get; set; }
-		public float tasa { get; set; }
-		public float monto { get; set; }
-		public bool pagado { get; set; }
-		public bool borrado { get; set; }
-		public DateTime fechaInicio { get; set; }
-		public DateTime fechaFin { get; set; }
-		public Usuario titular;
-
-
-		public PlazoFijo() { }
-
-		public PlazoFijo(int id, float monto, Usuario titular) {
-			this.id = id;
-			this.tasa = 0.2;
-			this.monto = monto;
-			this.titular = titular;
-			this.fechaInicio = DateTime.Now;
-			this.fechaInicio = DateTime.Now.AddMonths(1);
-			this.pagado = false;
-			this.borrado = false;
-		}
-
-		public string[] toArray() {
-			return new string[] {
-				this.tasa.ToString("C"),
-				this.monto.ToString("C"),
-				this.fechaInicio.ToString("G"),
-				this.fechaFin.ToString("G")
-			};
-		}
+namespace WinFormsApp1
+{
+    public partial class PlazoFijo : Form
+    {
+        public PlazoFijo()
+        {
+            InitializeComponent();
+        }
+    }
 }
---
+++ WinFormsApp1/TarjetaCreditoManager.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WinFormsApp1 {
+	public class TarjetaCredito
+	{
+		public int id { get; set; }
+		public int codigoV { get; set; }
+		public string numero { get; set; }
+		public float limite { get; set; }
+		public float consumo { get; set; }
+		public bool borrado { get; set; }
+		public int idUsuario { get; set }
+
+		public Usuario titular;
+
+
+		public TarjetaCredito() { }
+
+		public TarjetaCredito(int id, float limite, Usuario titular)
+		{
+			this.id = id;
+			Random rd = new Random();
+			this.numero = rd.Next(1000, 9999) + " " + rd.Next(1000, 9999) + " " + rd.Next(1000, 9999) + " " + rd.Next(1000, 9999);
+			this.codigoV = rd.Next(100, 999);
+			this.limite = limite;
+			this.consumo = 0;
+			this.titular = titular;
+		}
+
+        //Constructor para la conexión con BD
+        public TarjetaCredito(int id, int codigoV, int numero, float limite, float consumo, Boolean borrado, int idUsuario)
+        {
+            this.id = id;
+            Random rd = new Random();
+            this.numero = rd.Next(1000, 9999) + " " + rd.Next(1000, 9999) + " " + rd.Next(1000, 9999) + " " + rd.Next(1000, 9999);
+            this.codigoV = rd.Next(100, 999);
+            this.limite = limite;
+            this.consumo = 0;
+			this.borrado = borrado; 
+			this.idUsuario = idUsuario;
+        }
+
+        public string[] toArray()
+		{
+			return new string[] {
+				this.numero,
+				this.codigoV.ToString(),
+				this.limite.ToString("C"),
+				this.consumo.ToString("C")
+			};
+		}
+	}
+}
