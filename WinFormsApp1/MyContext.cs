bf8b3dc05b8a7f233bf92c3a1f69da7f5b20ef6d
Francisco Rivas <francisco.rivas@davinci.edu.ar>
2022-12-02 23:58:13 -0300

    creadas los restntes modelBuilders de las otras clases

--- WinFormsApp1/MyContext.cs
+++ WinFormsApp1/MyContext.cs
@@ -13,7 +13,7 @@ namespace WinFormsApp1
         public DbSet<CajaAhorro> cajaAhorro { get; set; }
         public DbSet<Movimiento> movimiento { get; set; }
         public DbSet<Pago> pago { get; set; }
-        public DbSet<PlazoFijo> plazoFiijo { get; set; }
+        public DbSet<PlazoFijo> plazoFijo { get; set; }
         public DbSet<TarjetaCredito> tarjetaCredito { get; set; }
 
         public MyContext() { } //está vacío porque el ConnectionString lo estamos guardando en el archivo Resources
@@ -79,7 +79,66 @@ namespace WinFormsApp1
                     mov.Property(m => m.idCajaAhorro).IsRequired(true);
                 });
 
-            
+            modelBuilder.Entity<Pago>()
+                .ToTable("Pago")
+                .HasKey(p => p.id_pago);
+
+            modelBuilder.Entity<Pago>(
+                pag =>
+                {
+                    pag.Property(p => p.nombre).HasColumnType("varchar(50)");
+                    pag.Property(p => p.nombre).IsRequired(true);
+                    pag.Property(p => p.monto).HasColumnType("float");
+                    pag.Property(p => p.monto).IsRequired(true);
+                    pag.Property(p => p.pagado).HasColumnType("bit");
+                    pag.Property(p => p.pagado).IsRequired(true);
+                    pag.Property(p => p.borrado).HasColumnType("bit");
+                    pag.Property(p => p.borrado).IsRequired(true);
+
+                });
+
+            modelBuilder.Entity<PlazoFijo>()
+                .ToTable("PlazoFijo")
+                .HasKey(f => f.id_plazo);
+
+            modelBuilder.Entity<PlazoFijo>(
+                plaz =>
+                {
+                    plaz.Property(f => f.monto).HasColumnType("varchar(50)");
+                    plaz.Property(f => f.monto).IsRequired(true);
+                    plaz.Property(f => f.fechaIni).HasColumnType("date");
+                    plaz.Property(f => f.fechaIni).IsRequired(true);
+                    plaz.Property(f => f.fechaFin).HasColumnType("date");
+                    plaz.Property(f => f.fechaFin).IsRequired(true);
+                    plaz.Property(f => f.tasa).HasColumnType("float");
+                    plaz.Property(f => f.tasa).IsRequired(true);
+                    plaz.Property(f => f.pagado).HasColumnType("bit");
+                    plaz.Property(f => f.pagado).IsRequired(true);
+                    plaz.Property(f => f.borrado).HasColumnType("bit");
+                    plaz.Property(f => f.borrado).IsRequired(true);
+
+                }
+                );
+
+            modelBuilder.Entity<TarjetaCredito>()
+                .ToTable("TarjetaCredito")
+                .HasKey(t => t.id_tarjeta);
+
+            modelBuilder.Entity<TarjetaCredito>(
+                tarj =>
+                {
+                    tarj.Property(t => t.codigoV).HasColumnType("int");
+                    tarj.Property(t => t.codigoV).IsRequired(true);
+                    tarj.Property(t => t.numero).HasColumnType("varchar(50)");
+                    tarj.Property(t => t.numero).IsRequired(true);
+                    tarj.Property(t => t.limite).HasColumnType("float");
+                    tarj.Property(t => t.limite).IsRequired(true);
+                    tarj.Property(t => t.consumo).HasColumnType("float");
+                    tarj.Property(t => t.consumo).IsRequired(true);
+                    tarj.Property(t => t.borrado).HasColumnType("bit");
+                    tarj.Property(t => t.borrado).IsRequired(true);
+
+                });
 
 
 
